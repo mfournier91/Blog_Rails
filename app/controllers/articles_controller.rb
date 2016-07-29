a53c574c7088bde6988a7aa77f5941787a7812f6
Marc Fournier <marc.fournier91@gmail.com>
2016-07-28 20:28:31 -0400

    Made validation for title length and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,18 +9,26 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 
 end
